e493e247305d5b265d1baefba034e4ace16b3429
Mathieu JOLY <m.joly@live.fr>
2021-12-24 13:45:08 +0100

    add form control

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,63 +1,65 @@
 import { useState, useEffect } from 'react';
 
 import show from './src/img/show.svg';
+import success from './src/img/success.svg';
+import error from './src/img/error.svg';
 
 import './Signup.scss';
 
 const Signup = () => {
 	const [formState, setFormState] = useState({name: "", email: "", password: "", confirmation: ""});
-	const [validState, setValidState] = useState({name: undefined, email: undefined, password: undefined, confirmation: undefined});
+	const [validState, setValidState] = useState({name: "", email: "", password: "", confirmation: ""});
 	const [displayPassword, setDisplayPassword] = useState(false)
 
 	const handleNameChange = (e) => {
 		const name = e.target.value
 		if (name === "") {
-			setValidState({ ...validState, name: undefined })
+			setValidState({ ...validState, name: "" })
 		} else if(/^[\w._-]{3,12}$/.test(name)) {
 			setFormState({ ...formState, name })
-			setValidState({ ...validState, name: true })
+			setValidState({ ...validState, name: "ok" })
 		} else {
-			console.log("name problem")
-			setValidState({ ...validState, name: false })
+			if(/[^\w._-]/.test(name)) setValidState({ ...validState, name: "no spacing or special characters" })
+			if(/^[\w._-]{1,2}$/.test(name)) setValidState({ ...validState, name: "minimum 3 characters" })
+			if(/^[\w._-]{13,}$/.test(name)) setValidState({ ...validState, name: "maximum 12 characters" })
 		}
 	}
 
 	const handleEmailChange = (e) => {
 		const email = e.target.value
 		if (email === "") {
-			setValidState({ ...validState, email: undefined })
-		} else if(/^[\w._-]{3,15}@[\w._-]{3,12}\.\w{2,5}(.\w{2,5})?$/.test(email)) {
+			setValidState({ ...validState, email: "" })
+		} else if(/^([\w_\.\-\+])+\@([\w\-]+\.)+([\w]{2,10})+$/.test(email)) {
 			setFormState({ ...formState, email })
-			setValidState({ ...validState, email: true })
+			setValidState({ ...validState, email: "ok" })
 		} else {
-			console.log("email problem")
-			setValidState({ ...validState, email: false })
+			setValidState({ ...validState, email: "not a valid format" })
 		}
 	}
 
 	const handlePasswordChange = (e) => {
 		const password = e.target.value
 		if (password === "") {
-			setValidState({ ...validState, password: undefined })
-		} else if(/^[\w._-]{8,15}$/.test(password)) {
+			setValidState({ ...validState, password: "" })
+		} else if(/^[\w._-$*+=&!?,]{8,15}$/.test(password)) {
 			setFormState({ ...formState, password })
-			setValidState({ ...validState, password: true })
+			setValidState({ ...validState, password: "ok" })
 		} else {
-			console.log("problem")
-			setValidState({ ...validState, password: false })
+			if(/[^\w._-$*+=&!?,]/.test(password)) setValidState({ ...validState, password: "no spacing or forbidden characters" })
+			if(/^[\w._-$*+=&!?,]{1,7}$/.test(password)) setValidState({ ...validState, password: "minimum 8 characters" })
+			if(/^[\w._-$*+=&!?,]{16,}$/.test(password)) setValidState({ ...validState, password: "maximum 15 characters" })
 		}
 	}
 
 	const handleConfirmationChange = (e) => {
 		const confirmation = e.target.value
 		if (confirmation === "") {
-			setValidState({ ...validState, confirmation: undefined })
+			setValidState({ ...validState, confirmation: "" })
 		} else if(confirmation === formState.password) {
 			setFormState({ ...formState, confirmation })
-			setValidState({ ...validState, confirmation: true })
+			setValidState({ ...validState, confirmation: "ok" })
 		} else {
-			console.log("problem")
-			setValidState({ ...validState, confirmation: false })
+			setValidState({ ...validState, confirmation: "doesn't match password" })
 		}
 	}
 
@@ -76,11 +78,16 @@ const Signup = () => {
 						name="name"
 						id="name"
 						placeholder=" "
-						style={{borderColor: `${validState.name === true ? "green" : validState.name === false ? "red" : "#D4D4D4"}`}}
+						style={{borderColor: `${validState.name === "ok" ? "green" : validState.name === "" ?  "#D4D4D4" : "red" }`}}
 					/>
 					<label htmlFor="name">Name</label>
-					<div className="Signup__form__field__error"></div>
-					<div className="Signup__form__field__success"></div>
+					<div className="Signup__form__field__error" style={{display: `${validState.name.length > 2 ? "block" : "none"}`}}>
+						<img className="Signup__form__field__error__image" src={error} alt="success logo" />
+						<p className="Signup__form__field__error__text">{validState.name}</p>
+					</div>
+					<div className="Signup__form__field__success" style={{display: `${validState.name === "ok" ? "block" : "none"}`}}>
+						<img src={success} alt="success logo" />
+					</div>
 				</div>
 				<div className="Signup__form__field">
 					<input 
@@ -89,11 +96,16 @@ const Signup = () => {
 						name="email"
 						id="email"
 						placeholder=" "
-						style={{borderColor: `${validState.email === true ? "green" : validState.email === false ? "red" : "#D4D4D4"}`}}
+						style={{borderColor: `${validState.email === "ok" ? "green" : validState.email === false ? "red" : "#D4D4D4"}`}}
 					/>
 					<label htmlFor="email">Email</label>
-					<div className="Signup__form__field__error"></div>
-					<div className="Signup__form__field__success"></div>
+					<div className="Signup__form__field__error" style={{display: `${validState.email.length > 2 ? "block" : "none"}`}}>
+						<img className="Signup__form__field__error__image" src={error} alt="success logo" />
+						<p className="Signup__form__field__error__text">{validState.email}</p>
+					</div>
+					<div className="Signup__form__field__success">
+						<img src={success} alt="success logo" style={{display: `${validState.email === "ok" ? "block" : "none"}`}} />
+					</div>
 				</div>
 				<div className="Signup__form__field">
 					<input 
@@ -101,14 +113,19 @@ const Signup = () => {
 						type={displayPassword ? "text" : "password"}
 						name="password" id="password"
 						placeholder=" "
-						style={{borderColor: `${validState.password === true ? "green" : validState.password === false ? "red" : "#D4D4D4"}`}}
+						style={{borderColor: `${validState.password === "ok" ? "green" : validState.password === false ? "red" : "#D4D4D4"}`}}
 					/>
 					<label htmlFor="password">Password</label>
 					<button className="Signup__form__field__show" onClick={() => setDisplayPassword(!displayPassword)}>
 						<img className="Signup__form__field__show__image" src={show} alt="eye symbol" />
 					</button>
-					<div className="Signup__form__field__error"></div>
-					<div className="Signup__form__field__success"></div>
+					<div className="Signup__form__field__error" style={{display: `${validState.password.length > 2 ? "block" : "none"}`}}>
+						<img className="Signup__form__field__error__image" src={error} alt="success logo" />
+						<p className="Signup__form__field__error__text">{validState.password}</p>
+					</div>
+					<div className="Signup__form__field__success" style={{display: `${validState.password === "ok" ? "block" : "none"}`}}>
+						<img src={success} alt="success logo" />
+					</div>
 				</div>
 				<div className="Signup__form__field">
 					<input 
@@ -117,14 +134,19 @@ const Signup = () => {
 						name="confirm-password"
 						id="confirm-password"
 						placeholder=" "
-						style={{borderColor: `${validState.confirmation === true ? "green" : validState.confirmation === false ? "red" : "#D4D4D4"}`}}
+						style={{borderColor: `${validState.confirmation === "ok" ? "green" : validState.confirmation === false ? "red" : "#D4D4D4"}`}}
 					/>
 					<label htmlFor="confirm-password">Confirm Password</label>
 					<button className="Signup__form__field__show" onClick={() => setDisplayPassword(!displayPassword)}>
 						<img className="Signup__form__field__show__image" src={show} alt="eye symbol" />
 					</button>
-					<div className="Signup__form__field__error"></div>
-					<div className="Signup__form__field__success"></div>
+					<div className="Signup__form__field__error" style={{display: `${validState.confirmation.length > 2 ? "block" : "none"}`}} >
+						<img className="Signup__form__field__error__image" src={error} alt="error logo" />
+						<p className="Signup__form__field__error__text">{validState.confirmation}</p>
+					</div>
+					<div className="Signup__form__field__success" style={{display: `${validState.confirmation === "ok" ? "block" : "none"}`}}>
+						<img src={success} alt="success logo"/>
+					</div>
 				</div>
 				<div className="Signup__form__field">
 					<button className="Signup__form__field__submit" onClick={() => handleSubmit()}>Submit</button>
