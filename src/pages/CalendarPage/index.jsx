d67943a8a7ea5e8bcc236c851b9b536da2166c83
Mathieu Joly <m.joly@live.fr>
2022-12-15 18:02:50 +0100

    add working calendar

--- src/pages/CalendarPage/index.jsx
+++ src/pages/CalendarPage/index.jsx
@@ -29,9 +29,36 @@ const getCurrentPeriod = () => {
   return new Date(`${year}-${month}-01`);
 };
 
+const getDateToString = (date) => date.toLocaleString().match(/^.*\d{4}/)[0];
+
+const getStartDay = (date) => {
+  let startDay = date;
+  while (startDay.getDay()) {
+    startDay.setDate(startDay.getDate() - 1);
+  }
+  return startDay;
+};
+
+const getWeekDays = (date) => {
+  const today = new Date();
+  let day = getStartDay(new Date(date.getTime()));
+
+  const days = [];
+  for (let i = 0; i < 5 * 7; i++) {
+    const dayObj = {
+      date: day.getMonth() === date.getMonth() ? "" + day.getDate() : "",
+      isToday: getDateToString(day) === getDateToString(today),
+    };
+    days.push(dayObj);
+    day.setDate(day.getDate() + 1);
+  }
+  return days;
+};
+
 const CalendarPage = () => {
   const [today] = useState(new Date());
   const [currentPeriod, setCurrentPeriod] = useState(getCurrentPeriod());
+  const [days, setDays] = useState(getWeekDays(new Date()))
 
   const handleNextMonth = () => {
     let month = currentPeriod.getMonth() + 1;
@@ -57,6 +84,10 @@ const CalendarPage = () => {
     setCurrentPeriod(new Date(`${year}-${month + 1}-01`));
   };
 
+  useEffect(() => {
+    setDays(getWeekDays(currentPeriod))
+  }, [currentPeriod])
+
   return (
     <div className="CalendarPage">
       <div className="CalendarPage__card">
@@ -75,6 +106,9 @@ const CalendarPage = () => {
             {WEEK_DAYS.map((weekDay, id) => (
               <li key={id}>{weekDay}</li>
             ))}
+          </ul>
+          <ul className="all-days">
+            {days.map((day, i) => <li key={i} className={day.isToday ? "is-today" : ""}>{day.date}</li>)}
           </ul>
         </main>
       </div>
